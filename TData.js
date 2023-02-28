4f167672b6d6e76e02e0289fd0f78d9ccbd2ed8c
AmondChocolateShake <dongju230130@gmail.com>
2023-02-28 14:59:26 +0900

    export

--- TData.js
+++ TData.js
@@ -1,6 +1,8 @@
-const trainingData={
+export const trainingData={
   title:"Lorem Ipsum",
   paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quibusdam laboriosam saepe soluta quia numquam ad expedita. Numquam, aliquid repellat, molestiae dignissimos porro beatae suscipit labore vero ullam molestias provident?",
   color:["#1E1E1E","#FFFFFF"],
 
 }
+
+
