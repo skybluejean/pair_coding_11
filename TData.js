21be9bdf7489a28e4acd2d2c4cfa4f4028fb1f9c
AmondChocolateShake <dongju230130@gmail.com>
2023-02-28 14:56:09 +0900

    interface added

---
+++ TData.js
@@ -0,0 +1,6 @@
+const trainingData={
+  title:"Lorem Ipsum",
+  paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quibusdam laboriosam saepe soluta quia numquam ad expedita. Numquam, aliquid repellat, molestiae dignissimos porro beatae suscipit labore vero ullam molestias provident?",
+  color:["#1E1E1E","#FFFFFF"],
+
+}
